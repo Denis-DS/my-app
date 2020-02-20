3b29234e4bb579344b94db533513e45329d170c0
Denis-DS <56930333+Denis-DS@users.noreply.github.com>
2020-02-20 20:07:43 +0200

    new

---
+++ build/precache-manifest.bcfe3b27098b4d5eefe68fa0bfe1bb0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2542ee6fb7879c5e9bbe6a2b7aa8ee17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d710eb25c4789a1d0776",
+    "url": "./static/css/main.220880d3.chunk.css"
+  },
+  {
+    "revision": "6b3f3f637756999e22f6",
+    "url": "./static/js/2.2dbaf0de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.2dbaf0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d710eb25c4789a1d0776",
+    "url": "./static/js/main.c223cfbe.chunk.js"
+  },
+  {
+    "revision": "567ca5cea940a0c54188",
+    "url": "./static/js/runtime-main.1a632c21.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "./static/media/252.e45d9d1f.png"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "./static/media/logoGithub.1bb02710.svg"
+  }
+]);
